6a06e62fa35ec6534e1d9a5ded9c0878b66a046e
Afrizal Wahyu Adi Putra <65825588+afrizalwahyuadi66@users.noreply.github.com>
2024-05-18 23:41:26 +0700

    Update UAS_ANS7.py

--- UAS_ANS7.py
+++ UAS_ANS7.py
@@ -1,6 +1,7 @@
 from U_File import PengelolaDataMahasiswa
 from U_Sort import U_Sort
 from U_Menu import main_menu
+from U_Menu import logo_main
 import U_Menu
 import U_File
 import os
@@ -8,13 +9,17 @@ import os
 def main():
     pengelola_mahasiswa = PengelolaDataMahasiswa()
     while True:
+        U_Menu.logo_main()
         U_Menu.main_menu()
         pilihan = input("Masukkan pilihan Anda: ")
 
         if pilihan == '1':
             os.system('cls' if os.name == 'nt' else 'clear')
+            U_Menu.logo_main()
             pengelola_mahasiswa.menu_pengelola_mahasiswa()
         elif pilihan == '2':
+            os.system('cls' if os.name == 'nt' else 'clear')
+            U_Menu.logo_main()
             U_Sort.sorting_menu()
         elif pilihan == '3':
             print("Program selesai.")
